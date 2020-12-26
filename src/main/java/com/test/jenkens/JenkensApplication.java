a5d92edfd04514d415230f9ccfa3671d1b557b03
yared2 <yared555@gmail.com>
2020-12-25 22:01:31 -0600

    first commit

---
+++ src/main/java/com/test/jenkens/JenkensApplication.java
@@ -0,0 +1,13 @@
+package com.test.jenkens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/jenkens/JenkensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.jenkens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
