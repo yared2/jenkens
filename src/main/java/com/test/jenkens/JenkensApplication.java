cfabd84413a517928115d5b21b57eb3c41a2b5c9
yared2 <yared555@gmail.com>
2020-12-25 22:21:34 -0600

    added later

--- src/main/java/com/test/jenkens/JenkensApplication.java
+++ src/main/java/com/test/jenkens/JenkensApplication.java
@@ -2,12 +2,19 @@ package com.test.jenkens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkensApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkensApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello() {
+		return " hi there added here";
+	}
 }
